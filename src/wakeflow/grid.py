Fixed planet rotation

src/wakeflow/grid.py
@@ -715,8 +715,8 @@ class _Grid:
         y_mesh = copy(self.Y[:,0,:])
         
         # rotation of phi_planet
-        rot_x_mesh =  np.cos(self.p.phi_planet) * x_mesh + np.sin(self.p.phi_planet) * y_mesh
-        rot_y_mesh = -np.sin(self.p.phi_planet) * x_mesh + np.cos(self.p.phi_planet) * y_mesh
+        rot_x_mesh =  np.cos(self.p.phi_planet_rad) * x_mesh + np.sin(self.p.phi_planet_rad) * y_mesh
+        rot_y_mesh = -np.sin(self.p.phi_planet_rad) * x_mesh + np.cos(self.p.phi_planet_rad) * y_mesh
         
         # interpolation functions
         v_r_func = RegularGridInterpolator(
@@ -756,7 +756,7 @@ class _Grid:
         v_phi_rot = v_phi_rot.reshape(v_phi.shape)
         rho_rot   = rho_rot  .reshape(rho.shape)
         
-        plt.imshow(v_r_rot, origin="lower")
+        plt.imshow(v_r_rot.T, origin="lower")
         plt.show()
         
         # add back to results

src/wakeflow/model_setup.py
@@ -57,10 +57,11 @@ class _Parameters(_Constants):
             self.m_planet       = None
             self.m_planet_array = list(config["m_planet"])
 
-        self.r_outer    = float(config["r_outer"])
-        self.r_inner    = float(config["r_inner"])
-        self.r_planet   = float(config["r_planet"])
-        self.phi_planet = float(config["phi_planet"])
+        self.r_outer        = float(config["r_outer"])
+        self.r_inner        = float(config["r_inner"])
+        self.r_planet       = float(config["r_planet"])
+        self.phi_planet_deg = float(config["phi_planet"])
+        self.phi_planet_rad = self.phi_planet_deg * np.pi / 180
         try:
             self.r_ref = float(config["r_ref"])
         except: